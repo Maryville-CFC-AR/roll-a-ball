89a38c47b19d05f3b70ff1b6d968ff699ca74fc3
Christoph <clorenz2@live.maryville.edu>
2018-02-21 16:44:39 -0600

    initcommit

---
+++ Assets/Editor/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    GameObject Ball;
+
+    private Vector3 Offset;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Offset = transform.position - Ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = Ball.transform.position + Offset;
+	}
+}
